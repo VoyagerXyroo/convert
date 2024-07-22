9ac00103c127568f6a0ebb6c20b64fc9dd1763c5
VoyagerXyroo <124270543+VoyagerXyroo@users.noreply.github.com>
2024-07-23 07:58:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
 
 @app.route('/')
 def index():
-    return render_template('convert.html')
+    return render_template('index.html')
 
 @app.route('/convert', methods=['POST'])
 def convert():
